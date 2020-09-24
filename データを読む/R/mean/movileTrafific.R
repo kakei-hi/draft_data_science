272e5c56916076a5d5ddb1ef5ff113af01374cc4
kakei-hi <61128445+kakei-hi@users.noreply.github.com>
2020-09-24 17:59:00 +0900

    update20200924
    ［データを読む］20200924

---
+++ データを読む/R/mean/movileTrafific.R
@@ -0,0 +1,26 @@
+# 【我が国の移動通信トラヒックの現状】
+
+traffic.vec = c(9.9, 23.4, 44.2, 80.0, 123.3, 184.5, 249.0, 335.9, 404.6, 442.3)
+ratio_traffic.vec = array(dim = 0)
+
+times = length(traffic.vec) - 1
+for (i in 1: times) {
+  ratio_traffic.vec[i] = traffic.vec[i + 1] / traffic.vec[i]
+}
+
+## 幾何平均
+geoMean = function(x){
+  
+  # 算術平均と幾何平均の関係を利用
+  g = exp(mean(log(x)))
+  
+  return(g)
+  
+}
+
+(ratio_traffic.vec = round(ratio_traffic.vec, 2))
+(g = geoMean(ratio_traffic.vec))
+(9.9 * g^(times))
+
+(a = mean(ratio_traffic.vec))
+(9.9 * a^(times))
